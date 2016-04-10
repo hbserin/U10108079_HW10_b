ee8a1e64011d6923be295a13b569c7aaecce3296
hbserin <hbs_erin@yahoo.com.tw>
2016-04-11 00:19:40 +0800

    fixed numbers in the list

--- bubbleSort.java
+++ bubbleSort.java
@@ -7,6 +7,22 @@
 public class bubbleSort {
 	public static void main(String[] args) {
 
-	  
+		int[] list ={10, 5, 8, 1, 4, 6, 14, 9, 12, 11};
+
+		for (int k =1; k < list.length ; k++) {
+			for (int i = 0; i <list.length - k; i++){
+				if (list[i] > list[i + 1]) {
+					//Swap list[i] with list[list =1]
+					int temp = list[i];
+					list[i] = list[i + 1];
+					list[i + 1] = temp;
+				}
+			}
+		}
+
+		for (int i = 0; i < list.length; i++)
+			System.out.print(list[i] + " ");
+
 	}
+
 }

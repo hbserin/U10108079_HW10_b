92f3d8126ee0a63124038372792bf2dae36c77ae
hbserin <hbs_erin@yahoo.com.tw>
2016-04-11 00:34:25 +0800

    can enter the numbers in the list

--- bubbleSort.java
+++ bubbleSort.java
@@ -3,11 +3,30 @@
  *Exercise:HW10_bubble sort
  */
 
+import java.util.Scanner;
+
 //main method
 public class bubbleSort {
 	public static void main(String[] args) {
 
-		int[] list ={10, 5, 8, 1, 4, 6, 14, 9, 12, 11};
+	  	// Create a Scanner
+		Scanner input = new Scanner(System.in);
+
+		//Enter how many numbers in the list
+		int number;
+		System.out.print("Enter how many numbers in the list：");
+		number= input.nextInt();		
+
+		//Create an array to store the list
+		int[] list;
+		list = new int[number];
+
+		//Enter the numbers in the list
+		System.out.println("Enter the numbers in the list：");
+		for(int i = 0; i<list.length; i++){
+			System.out.print("["+ (i+1) + "]:");
+			list[i] = input.nextInt();
+		}
 
 		for (int k =1; k < list.length ; k++) {
 			for (int i = 0; i <list.length - k; i++){
@@ -20,6 +39,8 @@ public class bubbleSort {
 			}
 		}
 
+		//Print the result
+		System.out.print("After bubble sort：");
 		for (int i = 0; i < list.length; i++)
 			System.out.print(list[i] + " ");
 
